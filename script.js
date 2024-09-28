12d41109f4796cd4b120f0bbe92a06fc8ff359e1
asrilHidayat404 <asril.code404@gmail.com>
2024-09-28 15:19:05 +0700

    mv to sc

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     console.log(element)
 }
+
+        console.log("Test 1")
+        const btn = document.querySelector("button")
+        const status = document.querySelector("p")
+        btn.addEventListener("click", (e) => {
+            for(let i = 0; i < 10; i++) {
+                console.log(i)
+            }
+            status.textContent = "ON" ? status.textContent = "OFF" : status.textContent = "ON"
+        })
+        console.log("Hello World")
+        let i = 10
+        var j = 20
+        var hasil = i + j
+        console.log(hasil)
